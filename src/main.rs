f6c945c27b42427a7f7cbf5b5ac5d6cb7631944b
hunterdw0007 <hunterw.mail@gmail.com>
2023-02-15 15:07:55 -0600

    feat: added sleep page and basic threading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fs,
+    net::{TcpListener, TcpStream},
+    fs,
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -9,7 +12,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,10 +22,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
